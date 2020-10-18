efc4e4f6960cddbeaa0de38735e230ec545cafc1
Blagovest-Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2020-10-18 13:52:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1.hourMinutesClock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int hour = 0;   //0:0  --- > 23 
+            //int minutes = 0; till 59 min -> change of hour
+
+            for (int hour = 0; hour < 24; hour++)
+            {
+                //hour ++;
+                for (int minutes = 0; minutes < 60; minutes++)
+                {
+                    Console.WriteLine($"{hour}:{minutes}");
+                }
+
+
+            }
+        }
+    }
+}
